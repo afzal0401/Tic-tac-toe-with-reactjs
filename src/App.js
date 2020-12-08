aff9a97abd7c9dbc877d0d3f9bf7f1262c5babfe
E-Sandhya <sandhya.e@etvbharat.com>
2020-12-08 16:41:18 +0530

    tic tac toe app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
+import Icon from "./components/Icon";
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import {Card, CardBody, Container, Button, Col, Row} from "reactstrap"
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './App.css';
 
-function App() {
+const itemArray = new Array(9).fill("empty")
+var arrayMaxValue = 0;
+
+const App = () => {
+
+const reloadGame = () => {
+  setIsCross(false)
+  setWinMessage("")
+  itemArray.fill("empty", 0, 9);
+}
+
+const checkIsWinner = () => {
+  // checking winner of the game
+ if (itemArray[0] ===itemArray[1] && itemArray[1] === itemArray[2] && itemArray[0] !== "empty" ) {
+    setWinMessage(`${itemArray[0]} wins`)
+    arrayMaxValue = 0;
+ } else if (itemArray[3] ===itemArray[4] && itemArray[4] === itemArray[5] && itemArray[3] !== "empty") {
+    setWinMessage(`${itemArray[3]} wins`)
+    arrayMaxValue = 0;
+ } else if (itemArray[6] ===itemArray[7] && itemArray[7] === itemArray[8] && itemArray[6] !== "empty") {
+    setWinMessage(`${itemArray[6]} wins`)
+    arrayMaxValue = 0;
+ } else if (itemArray[0] ===itemArray[3] && itemArray[3] === itemArray[6] && itemArray[0] !== "empty") {
+    setWinMessage(`${itemArray[0]} wins`)
+    arrayMaxValue = 0;
+ } else if (itemArray[1] ===itemArray[4] && itemArray[4] === itemArray[7] && itemArray[1] !== "empty") {
+    setWinMessage(`${itemArray[1]} wins`)
+    arrayMaxValue = 0;
+ } else if (itemArray[2] ===itemArray[5] && itemArray[5] === itemArray[8] && itemArray[2] !== "empty") {
+    setWinMessage(`${itemArray[2]} wins`)
+    arrayMaxValue = 0;
+ } else if (itemArray[0] ===itemArray[4] && itemArray[4] === itemArray[8] && itemArray[0] !== "empty") {
+    setWinMessage(`${itemArray[0]} wins`)
+    arrayMaxValue = 0;
+ } else if (itemArray[2] ===itemArray[4] && itemArray[4] === itemArray[6] && itemArray[2] !== "empty"){
+    setWinMessage(`${itemArray[2]} wins`)
+    arrayMaxValue = 0;
+ } else if(arrayMaxValue >= 9){
+    setWinMessage("Match tie");
+  arrayMaxValue = 0;
+ }
+}
+
+const changeItem = itemNumber => {
+  if (winMessage) {
+    return toast(winMessage, {type: "success"})
+  }
+
+  if (itemArray[itemNumber] === "empty") {
+    itemArray[itemNumber] = isCross ? "cross" : "circle"
+    setIsCross(!isCross)
+    arrayMaxValue++
+  } else {
+    return toast("alredy filled", {type: "error"})
+  }
+  if(arrayMaxValue <= 9 ){
+    checkIsWinner()
+  }
+} 
+
+const [isCross, setIsCross] = useState(false)
+const [winMessage, setWinMessage] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Container className="p-5">
+     <ToastContainer position="bottom-center"/>
+     <Row>
+       <Col md={6} className="offset-md-3">
+       { winMessage ? (
+         <div className="mb-2 mt-2">
+          <h1 className="text-success text-uppercase text-center">
+            {winMessage}
+          </h1>
+          <Button color="success" block onClick={reloadGame}> Reload game </Button>
+         </div>
+       ) : (
+          <h1 className="text-center text-warning">
+            {isCross ? "Cross" : "Circle"} turns
+          </h1>
+       )}
+        <div className="grid">
+          {itemArray.map((item, index) => (
+            <Card color="warning" onClick={ () => changeItem(index) }>
+              <CardBody className="box">
+                <Icon name={item} />
+              </CardBody>
+            </Card>
+          ))}
+        </div>
+       </Col>
+     </Row>
+   </Container>
   );
 }
 
